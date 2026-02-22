e01a00e002852e5293f4356a297240d8d4ac2585
Alexander <contact@nettoweb.ru>
2026-02-22 07:35:07 +0300

    list improvement

--- resources/views/components/ajaxlist.blade.php
+++ resources/views/components/ajaxlist.blade.php
@@ -48,52 +48,54 @@
                 <div class="result data">
                     {{ $slot }}
                 </div>
-                <div class="result bottom">
-                    <div class="table result-bottom-table">
-                        @if ($showNav)
-                            <div class="cell result-bottom-cell per-page">
-                                <label>
-                                    <select class="input text js-per-page disabled" disabled name="per-page" title="{{ __('main.title_per_page') }}">
-                                        @foreach ([10, 20, 50] as $item)
-                                            <option value="{{ $item }}">{{ format_number($item) }}</option>
-                                        @endforeach
-                                        <option value="0">{{ __('main.general_list_all') }}</option>
-                                    </select>
-                                </label>
-                            </div>
-                        @endif
-                        @if ($showTotal)
-                            <div class="cell result-bottom-cell total">
-                                <div>
-                                    <span class="text">{{ __('main.general_list_total') }}:</span>
+                @if ($showNav || $showTotal)
+                    <div class="result bottom">
+                        <div class="table result-bottom-table">
+                            @if ($showNav)
+                                <div class="cell result-bottom-cell per-page">
+                                    <label>
+                                        <select class="input text js-per-page disabled" disabled name="per-page" title="{{ __('main.title_per_page') }}">
+                                            @foreach ([10, 20, 50] as $item)
+                                                <option value="{{ $item }}">{{ format_number($item) }}</option>
+                                            @endforeach
+                                            <option value="0">{{ __('main.general_list_all') }}</option>
+                                        </select>
+                                    </label>
                                 </div>
-                                <div>
-                                    <span class="text js-total">0</span>
-                                </div>
-                            </div>
-                        @endif
-                        @if ($showNav)
-                            <div class="cell result-bottom-cell navigation">
-                                <div class="table result-bottom-nav-table">
-                                    <div class="cell result-bottom-nav-cell padding">
-                                        <span class="text desktop">{{ __('main.general_list_page') }}</span>
+                            @endif
+                            @if ($showTotal)
+                                <div class="cell result-bottom-cell total">
+                                    <div>
+                                        <span class="text">{{ __('main.general_list_total') }}:</span>
                                     </div>
-                                    <div class="cell result-bottom-nav-cell">
-                                        <label>
-                                            <select class="input text disabled js-page" disabled name="page"></select>
-                                        </label>
-                                    </div>
-                                    <div class="cell result-bottom-nav-cell padding">
-                                        <span class="text">/</span>
-                                    </div>
-                                    <div class="cell result-bottom-nav-cell">
-                                        <span class="text js-pages">{{ format_number(0) }}</span>
+                                    <div>
+                                        <span class="text js-total">0</span>
                                     </div>
                                 </div>
-                            </div>
-                        @endif
+                            @endif
+                            @if ($showNav)
+                                <div class="cell result-bottom-cell navigation">
+                                    <div class="table result-bottom-nav-table">
+                                        <div class="cell result-bottom-nav-cell padding">
+                                            <span class="text desktop">{{ __('main.general_list_page') }}</span>
+                                        </div>
+                                        <div class="cell result-bottom-nav-cell">
+                                            <label>
+                                                <select class="input text disabled js-page" disabled name="page"></select>
+                                            </label>
+                                        </div>
+                                        <div class="cell result-bottom-nav-cell padding">
+                                            <span class="text">/</span>
+                                        </div>
+                                        <div class="cell result-bottom-nav-cell">
+                                            <span class="text js-pages">{{ format_number(0) }}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endif
+                        </div>
                     </div>
-                </div>
+                @endif
             </div>
             <div class="data-block empty js-layer-results-empty">
                 <span class="text">
