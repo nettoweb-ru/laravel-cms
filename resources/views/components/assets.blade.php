348f65b3d225eadb5d6781f781e9baabac730e11
Alexander <contact@nettoweb.ru>
2026-02-27 11:38:23 +0300

    trailing slash, editor css

--- resources/views/components/assets.blade.php
+++ resources/views/components/assets.blade.php
@@ -1,7 +1,7 @@
 @use('Netto\Services\AssetService')
 <link rel="preconnect" href="{{ config('cms.assets.host') }}">
 @foreach (AssetService::css() as $file => $deferred)
-<link href="{{ config('cms.assets.host') }}/{{ $file }}" rel="stylesheet" @if ($deferred) media="print" @endif type="text/css" />
+<link href="{{ config('cms.assets.host') }}/{{ $file }}" rel="stylesheet" @if ($deferred) media="print" @endif type="text/css">
 @endforeach
 @foreach (AssetService::js() as $file => $deferred)
 <script @if ($deferred) defer @endif src="{{ config('cms.assets.host') }}/{{ $file }}"></script>
